650a8b3d1a895f9b0dd538717e92ed7ba5452136
Xavier <xavier.balita007@gmail.com>
2023-10-31 12:37:57 +0800

    Update mysqlconfig_connection.php

--- dbConnection/mysqlconfig_connection.php
+++ dbConnection/mysqlconfig_connection.php
@@ -2,7 +2,7 @@
     DEFINE('DB_USER', 'root');
     DEFINE('DB_PASSWORD', 'root');
     DEFINE('DB_HOST', 'localhost');
-    DEFINE('DB_NAME', 'db');
+    DEFINE('DB_NAME', 'dbschedule');
 
     $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
     OR dies('could not connect to MySQL: ' . mysql_connect_error());
